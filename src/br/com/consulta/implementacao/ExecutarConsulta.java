b287843721b7f559e4a21ddc83e32a7da4787d61
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-23 21:13:51 -0300

    Criando classe executavel

---
+++ src/br/com/consulta/implementacao/ExecutarConsulta.java
@@ -0,0 +1,17 @@
+package br.com.consulta.implementacao;
+
+import br.com.consulta.beans.Paciente;
+import br.com.consulta.util.input;
+
+public class ExecutarConsulta {
+
+	public static void main(String[] args) {
+		
+		Paciente pac = new Paciente();
+
+		pac.setNome(input.texto("Nome:"));
+		pac.setCpf(input.texto("CPF:"));
+		pac.setFone(input.texto("Telefone:"));
+	}
+
+}
